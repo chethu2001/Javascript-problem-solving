99edb4f98c5ea2bd1e9551f0cfe414843bffd97d
chethan <cehthanhk187@gmail.com>
2025-01-01 22:34:53 +0530

    problem solving

---
+++ Problem1.js
@@ -0,0 +1,32 @@
+//Question No.1
+
+//Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+// return the median of the two sorted arrays.
+
+let nums1 = [4];
+let nums2 = [];
+function News(nums1,nums2){
+    let g = nums1.concat(nums2);
+    
+    // console.log(g);
+    
+    if(g.length%2==0){
+        let mid = g.length / 2;
+        
+        let sum = g[mid-1] + g[mid];
+        let median = sum / 2;
+        return median.toFixed(5);
+        //console.log(median.toFixed(5));
+        }
+    else{
+        let mid = Math.ceil(g.length / 2);
+        return g[mid-1].toFixed(5);
+        // console.log(g[mid-1].toFixed(5))
+        
+    }
+
+
+
+}
+
+News(nums1,nums2);
